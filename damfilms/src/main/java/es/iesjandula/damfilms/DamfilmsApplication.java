c50a49c3de907a5fc6d77b807e8e1aaba008a648
JasonDGian <jasdavg@gmail.com>
2024-12-22 08:09:00 +0100

    DFT000 - Agrega proyecto base para con todas las dependencias.
    - Mueve los ficheros frontend en directorio 'backup'.
    - Agrega ficheros de proyecto Spring con depdencias.

---
+++ damfilms/src/main/java/es/iesjandula/damfilms/DamfilmsApplication.java
@@ -0,0 +1,13 @@
+package es.iesjandula.damfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamfilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamfilmsApplication.class, args);
+	}
+
+}
---
+++ damfilms/src/test/java/es/iesjandula/damfilms/DamfilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.damfilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamfilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
