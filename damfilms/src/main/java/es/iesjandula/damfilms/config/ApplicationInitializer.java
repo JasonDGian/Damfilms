319559a971e99ffe630aa2b362c804a630a4f699
JasonDGian <jasdavg@gmail.com>
2025-01-06 15:55:32 +0100

    DFT009.2 - Actualizar clase de inicializacion de aplicacion.
    Actualiza la clase de inicializacion de la aplicacion.
    - Reorganizad las sentencias de instanciacion de ficheros.
    - Introduce gestion de errores redirigiendo resultado a LOGs.
    - Documenta clase de inicializacion.

--- damfilms/src/main/java/es/iesjandula/damfilms/config/ApplicationInitializer.java
+++ damfilms/src/main/java/es/iesjandula/damfilms/config/ApplicationInitializer.java
@@ -1,68 +1,137 @@
-//package es.iesjandula.damfilms.config;
-//
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import es.iesjandula.damfilms.models.Role;
-//import es.iesjandula.damfilms.repositories.IRoleRepository;
-//import es.iesjandula.damfilms.utils.Constants;
-//import jakarta.annotation.PostConstruct;
-//import lombok.extern.slf4j.Slf4j;
-//
-////@Service
-//@Slf4j
-//public class ApplicationInitializer
-//{
-//	// Inyecta dependencia de repositorio de roles.
-//	@Autowired
-//	IRoleRepository iRoleRepository;
-//
-//	/**
-//	 * Metodo de inicialización de la aplicacion que controla si los roles
-//
-//	 * necesarios para el funcionamiento existen en BBDD, de no existir los agrega.
-//	 * Se apoya en metodo privado saveRoleOperation()
-//	 * 
-//	 * @see saveRoleOperation()
-//	 */
-//	@PostConstruct
-//	public void createRoles()
-//	{
-//
-//		// ------------------------ ROLE INVITADO ------------------------------------
-//		saveRoleOperation(Constants.ROLE_GUEST_USER);
-//		// ------------------------ ROLE BASICO ------------------------------------
-//		saveRoleOperation(Constants.ROLE_BASIC_USER);
-//		// ------------------------ ROLE ESTANDAR ------------------------------------
-//		saveRoleOperation(Constants.ROLE_STANDARD_USER);
-//		// ------------------------ ROLE PREMIUM ------------------------------------
-//		saveRoleOperation(Constants.ROLE_PREMIUM_USER);
-//	}
-//
-//	/**
-//	 * Metodo que recibiendo una string de un rol controla si existe en bases de
-//	 * datos y de no existir lo almacenará.
-//	 * 
-//	 * @param roleString
-//	 */
-//	private void saveRoleOperation(String roleString)
-//	{
-//		// Busca objeto rol BASICO en BBDD.
-//		log.debug("{} - Buscando rol - {}", Constants.DEBUG_TAG, roleString);
-//		Optional<Role> roleOpt = this.iRoleRepository.findById(roleString);
-//
-//		// Si el objeto opcional recuperado está vacio ( el rol no persiste en bbdd )
-//		if (roleOpt.isEmpty())
-//		{
-//			// Almacena el rol en BBDD.
-//			this.iRoleRepository.saveAndFlush(new Role(roleString, null));
-//			// Informa en debug de la operación.
-//			log.debug("{} - Rol almacenado en BBDD - {}", Constants.DEBUG_TAG, roleString);
-//		}
-//		else {
-//			log.debug("{} - Rol ya existe en BBDD - {}", Constants.DEBUG_TAG, roleString);
-//		}
-//	}
-//}
+package es.iesjandula.damfilms.config;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.iesjandula.damfilms.services.parsers.IParseoDocumental;
+import es.iesjandula.damfilms.services.parsers.IParseoGenero;
+import es.iesjandula.damfilms.services.parsers.IParseoPelicula;
+import es.iesjandula.damfilms.services.parsers.IParseoSerie;
+import es.iesjandula.damfilms.services.parsers.IParseoSuscripcion;
+import es.iesjandula.damfilms.services.parsers.IParseoTipo;
+import es.iesjandula.damfilms.services.parsers.iParseoGeneroPelicula;
+import es.iesjandula.damfilms.services.parsers.iParseoGeneroSerie;
+import es.iesjandula.damfilms.services.parsers.iParseoTemporada;
+import es.iesjandula.damfilms.utils.Constants;
+import es.iesjandula.damfilms.utils.DamfilmsServerError;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ApplicationInitializer
+{
+	// Inyecta dependencia de repositorio de roles.
+	@Autowired
+	private IParseoPelicula iParseoPelicula;
+
+	@Autowired
+	private IParseoDocumental iParseoDocumental;
+
+	@Autowired
+	private IParseoGenero iParseoGenero;
+
+	@Autowired
+	private IParseoSerie iParseoSerie;
+
+	@Autowired
+	private IParseoTipo iParseoTipo;
+
+	@Autowired
+	private iParseoGeneroPelicula iParseoGeneroPelicula;
+
+	@Autowired
+	private iParseoGeneroSerie iParseoGeneroSerie;
+
+	@Autowired
+	private iParseoTemporada iParseoTemporada;
+
+	@Autowired
+	private IParseoSuscripcion iParseoSuscripcion;
+
+	/**
+     * Este método se ejecuta automáticamente tras la inicialización de la aplicacion.
+     * Se encarga de cargar los datos iniciales desde archivos CSV.
+     *  
+     * @throws DamfilmsServerError Si ocurre un error en el servidor durante el proceso.
+     * @throws FileNotFoundException Si alguno de los archivos CSV no se encuentra.
+     */
+	@PostConstruct
+	@Transactional(readOnly = false)
+	public void parsea()
+	{
+		
+		try {
+			// Define la ruta comun a todos los ficheros CSV.
+			String ruta = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static"
+					+ File.separator + "csv" + File.separator;
+	
+			// Define los objetos de ficheros CSV.
+			File fileGeneros = new File(ruta + "generos.csv");
+			File fileTipos = new File(ruta + "tipos.csv");
+			File fileSeries = new File(ruta + "series.csv");
+			File fileTemporadas = new File(ruta + "temporadas.csv");
+			File filePeliculas = new File(ruta + "peliculas.csv");
+			File fileDocumentales = new File(ruta + "documentales.csv");
+			File fileGeneroPeliculas = new File(ruta + "generoPeliculas.csv");
+			File fileGeneroSeries = new File(ruta + "generoSeries.csv");
+			File fileSuscripciones = new File(ruta + "suscripciones.csv");
+	
+			// Bloque de parseo.
+			try (Scanner scannerGeneros = new Scanner(fileGeneros))
+			{
+				iParseoGenero.parseaFicheros(scannerGeneros);
+			}
+	
+			try (Scanner scannerTipo = new Scanner(fileTipos))
+			{
+				iParseoTipo.parseaFicheros(scannerTipo);
+			}
+	
+			try (Scanner scannerSeries = new Scanner(fileSeries))
+			{
+				iParseoSerie.parseaFicheros(scannerSeries);
+			}
+	
+			try (Scanner scannerTemporadas = new Scanner(fileTemporadas))
+			{
+				iParseoTemporada.parseaFicheros(scannerTemporadas);
+			}
+	
+			try (Scanner scannerPeliculas = new Scanner(filePeliculas))
+			{
+				iParseoPelicula.parseaFicheros(scannerPeliculas);
+			}
+	
+			try (Scanner scannerDocumentales = new Scanner(fileDocumentales))
+			{
+				iParseoDocumental.parseaFicheros(scannerDocumentales);
+			}
+	
+			try (Scanner scannerGeneroPeliculas = new Scanner(fileGeneroPeliculas))
+			{
+				iParseoGeneroPelicula.parseaFicheros(scannerGeneroPeliculas);
+			}
+	
+			try (Scanner scannerGeneroSeries = new Scanner(fileGeneroSeries))
+			{
+				iParseoGeneroSerie.parseaFicheros(scannerGeneroSeries);
+			}
+	
+			try (Scanner scannerSuscripciones = new Scanner(fileSuscripciones))
+			{
+				iParseoSuscripcion.parseaFicheros(scannerSuscripciones);
+			}
+		
+		}
+		catch ( Exception e ){
+			log.error( "{} - ERROR EN MECANISMO DE PARSEO: {}", Constants.DEBUG_TAG, e.getMessage() );
+		}
+
+	}
+}
